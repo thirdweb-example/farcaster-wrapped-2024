ddf0e70b9c09575a8393c8cdfd1e7342b67f12e5
gregfromstl <gregfromstl@gmail.com>
2024-12-25 10:31:30 -0600

    remove wallet connection

--- src/hooks/useWarpcast.ts
+++ src/hooks/useWarpcast.ts
@@ -1,21 +1,9 @@
-import { useEffect, useMemo, useState } from "react";
-import { Account, EIP1193 } from "thirdweb/wallets";
+import { useEffect, useState } from "react";
 import sdk, { type FrameContext } from "@farcaster/frame-sdk";
-import { ThirdwebClient } from "~/constants";
 
 export function useWarpcast() {
   const [context, setContext] = useState<FrameContext>();
   const [isLoading, setIsLoading] = useState(true);
-  const [account, setAccount] = useState<Account>();
-
-  const wallet = useMemo(() => {
-    if (!isLoading && sdk.wallet) {
-      try {
-        return EIP1193.fromProvider({ provider: sdk.wallet.ethProvider });
-      } catch { }
-    }
-    return undefined;
-  }, [sdk.wallet, isLoading]);
 
   useEffect(() => {
     const load = async () => {
@@ -27,18 +15,5 @@ export function useWarpcast() {
     }
   }, [isLoading]);
 
-  // Autoconnect
-  useEffect(() => {
-    if (wallet) {
-      connect();
-    }
-  }, [wallet]);
-
-  const connect = async () => {
-    if (typeof wallet !== "undefined") {
-      wallet.connect({ client: ThirdwebClient }).then(setAccount);
-    }
-  };
-
-  return { wallet, account, connect, user: context?.user, openUrl: sdk.actions.openUrl, isLoading };
+  return { user: context?.user, openUrl: sdk.actions.openUrl, isLoading };
 }
